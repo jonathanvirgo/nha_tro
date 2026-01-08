f2f220fac470995c482c92933b94ffac74bc1b39
QD <qdatvirgo@gmail.com>
2026-01-08 16:28:02 +0700

    test build cloudflare page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',  // Bắt buộc cho static export
+  distDir: 'out',
 };
 
 export default nextConfig;
