653e2ecaff1a959bd2a5459982601f81b0fbd164
QD <qdatvirgo@gmail.com>
2026-01-08 16:34:44 +0700

    add st

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',  // Bắt buộc cho static export
-  distDir: 'out',
 };
 
 export default nextConfig;
